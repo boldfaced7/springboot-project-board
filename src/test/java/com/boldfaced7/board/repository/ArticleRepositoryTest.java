d77ed0a5d1b2543ea03cdb4c62d79c3c5d7fcd25
boldfaced7 <boldfaced753@gmail.com>
2024-02-08 19:22:37 +0900

    #1 - ArticleRepository 기능 개발
    - ArticleRepositoryTest 클래스 작성
    - Spring Data JPA를 이용해, ArticleRepository 인터페이스 작성

---
+++ src/test/java/com/boldfaced7/board/repository/ArticleRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.boldfaced7.board.repository;
+
+import com.boldfaced7.board.config.JpaConfig;
+import com.boldfaced7.board.domain.Article;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("ArticleRepository 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ArticleRepositoryTest {
+
+    @Autowired private ArticleRepository articleRepository;
+
+    private Article article;
+
+    @BeforeEach
+    void setup() {
+        article = Article.builder().content("test").title("test").build();
+    }
+
+
+    @DisplayName("findAll()이 isActive가 true인 Article 객체를 반환하는지 확인")
+    @Test
+    void givenArticle_whenSelecting_thenWorksFine() {
+        // Given
+        articleRepository.save(article);
+
+        // When
+        List<Article> articles = articleRepository.findAll();
+
+        //Then
+        assertThat(articles).hasSize(1);
+        assertThat(articles.get(0).getId()).isEqualTo(article.getId());
+        assertThat(articles.get(0).getTitle()).isEqualTo(article.getTitle());
+        assertThat(articles.get(0).getContent()).isEqualTo(article.getContent());
+        assertThat(articles.get(0).isActive()).isTrue();
+    }
+
+    @DisplayName("findAll()이 isActive가 false인 Article 객체를 반환하지 않는지 확인")
+    @Test
+    void givenDeletedArticle_whenSelecting_thenWorksFine() {
+        // Given
+        articleRepository.save(article);
+        article.deactivate();
+
+        // When
+        List<Article> articles = articleRepository.findAllByIsActiveIsTrue();
+
+        //Then
+        assertThat(articles).hasSize(0);
+    }
+}

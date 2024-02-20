8fbd3ceba3e8b5752dbbf973ec60c7d4fdf04184
boldfaced7 <boldfaced753@gmail.com>
2024-02-20 14:00:15 +0900

    #5 - Member 도메인 클래스 작성
    - 회원에 필요한 필드를 정의
    - JPA 엔티티로 사용할 수 있도록 클래스 작성

---
+++ src/main/java/com/boldfaced7/board/domain/Member.java
@@ -0,0 +1,66 @@
+package com.boldfaced7.board.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Getter
+@Entity
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    private boolean isActive = true;
+
+    protected Member() {}
+
+    @Builder
+    public Member(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void update(Member member) {
+        if (member.getNickname() != null) updateNickname(member.getNickname());
+    }
+
+    public void deactivate() {
+        isActive = false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

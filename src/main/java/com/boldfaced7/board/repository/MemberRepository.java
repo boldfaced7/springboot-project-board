a96ab095fc2c8711de0294667357d4fbd87de605
boldfaced7 <boldfaced753@gmail.com>
2024-02-20 14:01:12 +0900

    #5 - MemberRepository 기능 개발
    - MemberRepositoryTest 클래스 작성
    - Spring Data JPA(네임드 쿼리, @Query)를 이용해, 테스트를 통과하는 MemberRepository 인터페이스 작성

---
+++ src/main/java/com/boldfaced7/board/repository/MemberRepository.java
@@ -0,0 +1,28 @@
+package com.boldfaced7.board.repository;
+
+import com.boldfaced7.board.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    public Member save(Member member);
+
+    @Query("select m from Member m")
+    public List<Member> findAll();
+
+    @Query("select m from Member m where m.isActive = :isActive")
+    public List<Member> findAll(@Param("isActive") boolean isActive);
+
+    @Query("select m from Member m where m.id = :id")
+    public Optional<Member> findById(Long id);
+
+    public void delete(Member member);
+
+    public boolean existsByEmail(String email);
+
+    public boolean existsByNickname(String nickname);
+}

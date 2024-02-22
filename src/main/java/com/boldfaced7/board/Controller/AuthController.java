f91bf3758488f160fb4c3723a094aa86ce6edfd0
boldfaced7 <boldfaced753@gmail.com>
2024-02-22 23:20:25 +0900

    #7 - AuthController 기능 개발
    - Controller 레이어에서 사용할 DTO인 AuthRequest, AuthResponse 클래스 작성
    - AuthController를 검증할 AuthControllerTest 코드 작성
    - 테스트 코드를 통과하는 AuthController 클래스 작성

---
+++ src/main/java/com/boldfaced7/board/Controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.boldfaced7.board.Controller;
+
+import com.boldfaced7.board.dto.AuthDto;
+import com.boldfaced7.board.dto.request.AuthRequest;
+import com.boldfaced7.board.dto.response.AuthResponse;
+import com.boldfaced7.board.auth.SessionConst;
+import com.boldfaced7.board.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api")
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(
+            @RequestBody AuthRequest authRequest,
+            HttpSession session) {
+
+        AuthDto login = authService.login(authRequest.toDto());
+        AuthResponse authResponse = new AuthResponse(login);
+
+        session.setMaxInactiveInterval(1800);
+        session.setAttribute(SessionConst.AUTH_RESPONSE, authResponse);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return ResponseEntity.ok().build();
+    }
+}

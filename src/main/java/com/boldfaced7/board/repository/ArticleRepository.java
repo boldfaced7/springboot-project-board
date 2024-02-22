1aef0d7e682a5a076f08a556361fefdd0bdb7957
boldfaced7 <boldfaced753@gmail.com>
2024-02-22 23:25:47 +0900

    #7 - ArticleRepository, ArticleCommentRepository 수정
    - Member 도입을 위해 ArticleRepositoryTest, ArticleCommentRepositoryTest 작성
    - 테스트를 통과하는 ArticleRepository, ArticleCommentRepository 작성

--- src/main/java/com/boldfaced7/board/repository/ArticleRepository.java
+++ src/main/java/com/boldfaced7/board/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package com.boldfaced7.board.repository;
 
 import com.boldfaced7.board.domain.Article;
+import com.boldfaced7.board.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +11,8 @@ import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     public Article save(Article article);
+
+    public void delete(Article article);
 
     @Query("select a from Article a where a.id = :id and a.isActive = true")
     public Optional<Article> findById(@Param("id") Long id);
@@ -21,5 +24,8 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     public List<Article> findAllByIsActiveIsTrue();
 
-    public void delete(Article article);
+    @Query("select a from Article a where a.member = :member and a.isActive = true")
+    public List<Article> findAllByMember(@Param("member") Member member);
+
+    public List<Article> findAllByMemberAndIsActiveIsTrue(Member member);
 }
--- src/test/java/com/boldfaced7/board/repository/ArticleRepositoryTest.java
+++ src/test/java/com/boldfaced7/board/repository/ArticleRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.boldfaced7.board.repository;
 
+import com.boldfaced7.board.TestUtil;
 import com.boldfaced7.board.config.JpaConfig;
 import com.boldfaced7.board.domain.Article;
+import com.boldfaced7.board.domain.ArticleComment;
+import com.boldfaced7.board.domain.Member;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +15,7 @@ import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
+import static com.boldfaced7.board.TestUtil.*;
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("ArticleRepository 테스트")
@@ -21,18 +25,13 @@ import static org.assertj.core.api.Assertions.*;
 class ArticleRepositoryTest {
 
     @Autowired private ArticleRepository articleRepository;
-
-    private Article article;
-
-    @BeforeEach
-    void setup() {
-        article = Article.builder().content("test").title("test").build();
-    }
+    @Autowired private MemberRepository memberRepository;
 
     @DisplayName("findAll()이 isActive가 true인 Article 객체를 반환하는지 확인")
     @Test
     void givenArticle_whenSelecting_thenWorksFine() {
         // Given
+        Article article = createArticle();
         articleRepository.save(article);
 
         // When
@@ -50,6 +49,7 @@ class ArticleRepositoryTest {
     @Test
     void givenDeletedArticle_whenSelecting_thenWorksFine() {
         // Given
+        Article article = createArticle();
         articleRepository.save(article);
         article.deactivate();
 
@@ -58,5 +58,57 @@ class ArticleRepositoryTest {
 
         //Then
         assertThat(articles).hasSize(0);
+    }
+
+    @DisplayName("findAllByMember()가 isActive가 true인 Article 객체를 반환하는지 확인")
+    @Test
+    void givenMemberAndArticle_whenSelecting_thenWorksFine() {
+        //Given
+        Member member = memberRepository.save(createMember());
+        Article article = articleRepository.save(createArticle(member));
+
+        // When
+        List<Article> members = articleRepository.findAllByMember(member);
+
+        // Then
+        assertThat(members.get(0).isActive()).isTrue();
+    }
+
+    @DisplayName("findAllByMember()가 isActive가 false인 Article 객체를 반환하지 않는지 확인")
+    @Test
+    void givenMemberAndInactiveArticle_whenSelecting_thenWorksFine() {
+        //Given
+        Member member = memberRepository.save(createMember());
+        Article article = articleRepository.save(createArticle(member));
+        article.deactivate();
+
+        // When
+        List<Article> members = articleRepository.findAllByMember(member);
+
+        // Then
+        assertThat(members).isEmpty();
+    }
+
+    private Article createArticle() {
+        return Article.builder()
+                .title(TITLE)
+                .content(CONTENT)
+                .build();
+    }
+
+    private Article createArticle(Member member) {
+        return Article.builder()
+                .title(TITLE)
+                .content(CONTENT)
+                .member(member)
+                .build();
+    }
+
+    private Member createMember() {
+        return Member.builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .nickname(NICKNAME)
+                .build();
     }
 }

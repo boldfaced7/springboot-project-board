545e2418f0e7577ffe8ccb4ecf352d74916b19ee
boldfaced7 <boldfaced753@gmail.com>
2024-03-15 15:51:13 +0900

    #11 - 기반 클래스 작성
    - @ParameterizedTest 기능을 확장해 테스트 코드 내 중복을 제거하기 위해 필요한 클래스를 작성
    - 모킹 객체를 참조할 수 없는 정적 메소드에서 반환된 값에 모킹 객체를 주입하는 것이 핵심

---
+++ src/test/java/com/boldfaced7/board/GivenAndThen.java
@@ -0,0 +1,98 @@
+package com.boldfaced7.board;
+
+import lombok.*;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.then;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GivenAndThen {
+    private Runnable given;
+    private Runnable then;
+
+    public <P, T> GivenAndThen(T entity, BiConsumer<T, P> method, P param) {
+        given = () -> method.accept(willDoNothing().given(entity), param);
+        then = () -> method.accept(then(entity).should(), param);
+    }
+
+    public <P, T, E extends Exception> GivenAndThen(T entity, BiConsumer<T, P> method, P param, E exception) {
+        given = () -> method.accept(willThrow(exception).given(entity), param);
+        then = () -> method.accept(then(entity).should(), param);
+    }
+
+    public <R, T> GivenAndThen(T entity, Function<T, R> method, R result) {
+        given = () -> given(method.apply(entity)).willReturn(result);
+        then = () -> method.apply(then(entity).should());
+    }
+
+    public <R, T, E extends Exception> GivenAndThen(T entity, Function<T, R> method, E exception) {
+        given = () -> method.apply(willThrow(exception).given(entity));
+        then = () -> method.apply(then(entity).should());
+    }
+
+    public <P, R, T> GivenAndThen(T entity, BiFunction<T, P, R> method, P param, R result) {
+        given = () -> given(method.apply(entity, param)).willReturn(result);
+        then = () -> method.apply(then(entity).should(), param);
+    }
+
+    public <P, R, T, E extends Exception> GivenAndThen(T entity, BiFunction<T, P, R> method, P param, E exception) {
+        given = () -> method.apply(willThrow(exception).given(entity), param);
+        then = () -> method.apply(then(entity).should(), param);
+    }
+
+    public <P, S, T> GivenAndThen(T entity, BiConsumer<S, P> method, P param, Function<T, S> holder) {
+        S covered = holder.apply(entity);
+        given = () -> method.accept(willDoNothing().given(covered), param);
+        then = () -> method.accept(then(covered).should(), param);
+    }
+
+    public <P, S, T, E extends Exception> GivenAndThen(T entity, BiConsumer<S, P> method, P param, E exception, Function<T, S> holder) {
+        S covered = holder.apply(entity);
+        given = () -> method.accept(willThrow(exception).given(covered), param);
+        then = () -> method.accept(then(covered).should(), param);
+    }
+
+    public <R, S, T> GivenAndThen(T entity, Function<S, R> method, R result, Function<T, S> holder) {
+        S covered = holder.apply(entity);
+        given = () -> given(method.apply(covered)).willReturn(result);
+        then = () -> method.apply(then(covered).should());
+    }
+
+    public <E extends Exception, R, S, T> GivenAndThen(T entity, Function<S, R> method, E exception, Function<T, S> holder) {
+        S covered = holder.apply(entity);
+        given = () -> method.apply(willThrow(exception).given(covered));
+        then = () -> method.apply(then(covered).should());
+    }
+
+    public <P, R, S, T> GivenAndThen(T entity, BiFunction<S, P, R> method, P param, R result, Function<T, S> holder) {
+        S covered = holder.apply(entity);
+        given = () -> given(method.apply(covered, param)).willReturn(result);
+        then = () -> method.apply(then(covered).should(), param);
+    }
+
+    public <P, R, S, T, E extends Exception> GivenAndThen(T entity, BiFunction<S, P, R> method, P param, E exception, Function<T, S> holder) {
+        S covered = holder.apply(entity);
+        given = () -> method.apply(willThrow(exception).given(covered), param);
+        then = () -> method.apply(then(covered).should(), param);
+    }
+
+    public <R, T, U> Function<T, R> cover(Function<U, R> target, Function<T, U> holder) {
+        return t -> target.apply(holder.apply(t));
+    }
+
+    public <P, R, T, U> BiFunction<T, P, R> cover(BiFunction<U, P, R> target, Function<T, U> holder) {
+        return (t, param) -> target.apply(holder.apply(t), param);
+    }
+
+    public <P, T, U> BiConsumer<T, P> cover(BiConsumer<U, P> target, Function<T, U> holder) {
+        return (t, param) -> target.accept(holder.apply(t), param);
+    }
+}

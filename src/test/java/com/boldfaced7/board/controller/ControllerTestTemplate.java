2a3a961824113b9933b02394e943988f8739d8ab
boldfaced7 <boldfaced753@gmail.com>
2024-03-19 15:46:33 +0900

    #13 - AttachmentController 기능 개발
    - Controller 레이어에서 사용할 DTO 작성
    - AttachmentController를 검증할 AttachmentControllerTest 작성
    - 테스트 코드 템플릿이 MultiPartFile을 사용할 수 있도록 수정
    - 테스트 코드를 통과하는 AttachmentController 클래스 작성

--- src/test/java/com/boldfaced7/board/controller/ControllerTestTemplate.java
+++ src/test/java/com/boldfaced7/board/controller/ControllerTestTemplate.java
@@ -9,10 +9,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
 
 import java.util.List;
 
@@ -29,14 +31,46 @@ class ControllerTestTemplate<S> {
     MockHttpSession session;
     S service;
 
-    public <T> ResultActions mvcPerform(MockHttpServletRequestBuilder requestBuilder, T request) throws Exception {
-        return mvc.perform(requestBuilder
-                        .session(session)
-                        .content(gson.toJson(request))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print());
+    public void doMultipart(Context<S> context, String url, List<MockMultipartFile> files, List<ResultMatcher> resultMatchers) throws Exception {
+
+        GivenAndThen givenAndThen = context != null ? context.convert(service) : null;
+
+        // Given
+        if (givenAndThen != null && givenAndThen.getGiven() != null) {
+            givenAndThen.getGiven().run();
+        }
+
+        // When
+        MockMultipartHttpServletRequestBuilder multipart = multipart(url);
+        for (MockMultipartFile file : files) {
+            multipart = multipart.file(file);
+        }
+
+        ResultActions resultActions = mvc.perform(multipart.session(session)).andDo(print());
+
+        // Then
+        for (ResultMatcher resultMatcher: resultMatchers) {
+            resultActions.andExpect(resultMatcher);
+        }
+
+        if (givenAndThen != null && givenAndThen.getThen() != null) {
+            givenAndThen.getThen().run();
+        }
     }
-    <T> void performRequest(Context<S> context, MockHttpServletRequestBuilder requestBuilder, T request, List<ResultMatcher> resultMatchers) throws Exception {
+
+    public void doGet(Context<S> context, String url, List<ResultMatcher> validators) throws Exception {
+        performRequest(context, get(url), null, validators);
+    }
+    public <T> void doPost(Context<S> context, String url, T request, List<ResultMatcher> validators) throws Exception {
+        performRequest(context, post(url), request, validators);
+    }
+    public <T> void doPatch(Context<S> context, String url, T request, List<ResultMatcher> validators) throws Exception {
+        performRequest(context, patch(url), request, validators);
+    }
+    public void doDelete(Context<S> context, String url, List<ResultMatcher> validators) throws Exception {
+        performRequest(context, delete(url), null, validators);
+    }
+    private <T> void performRequest(Context<S> context, MockHttpServletRequestBuilder requestBuilder, T request, List<ResultMatcher> resultMatchers) throws Exception {
         GivenAndThen givenAndThen = context != null ? context.convert(service) : null;
 
         // Given
@@ -56,19 +90,11 @@ class ControllerTestTemplate<S> {
             givenAndThen.getThen().run();
         }
     }
-    void doGet(Context<S> context, String url, List<ResultMatcher> validators) throws Exception {
-        performRequest(context, get(url), null, validators);
-    }
-
-    <T> void doPost(Context<S> context, String url, T request, List<ResultMatcher> validators) throws Exception {
-        performRequest(context, post(url), request, validators);
-    }
-
-    <T> void doPatch(Context<S> context, String url, T request, List<ResultMatcher> validators) throws Exception {
-        performRequest(context, patch(url), request, validators);
-    }
-
-    void doDelete(Context<S> context, String url, List<ResultMatcher> validators) throws Exception {
-        performRequest(context, delete(url), null, validators);
+    private <T> ResultActions mvcPerform(MockHttpServletRequestBuilder requestBuilder, T request) throws Exception {
+        return mvc.perform(requestBuilder
+                        .session(session)
+                        .content(gson.toJson(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
     }
 }

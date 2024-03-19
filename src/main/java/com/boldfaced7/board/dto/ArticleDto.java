5febb76dd67a236923e08dd032685383692f2275
boldfaced7 <boldfaced753@gmail.com>
2024-03-19 15:44:31 +0900

    #13 - ArticleService 기능 추가
    - 게시글 관련 기능에 개발한 첨부파일 관련 기능을 추가하도록 테스트 코드와 DTO를 작성
    - 작성한 테스트 코드를 통과하도록 ArticleService 수정

--- src/main/java/com/boldfaced7/board/dto/ArticleDto.java
+++ src/main/java/com/boldfaced7/board/dto/ArticleDto.java
@@ -2,8 +2,8 @@ package com.boldfaced7.board.dto;
 
 import com.boldfaced7.board.domain.Article;
 import com.boldfaced7.board.domain.ArticleComment;
+import com.boldfaced7.board.domain.Attachment;
 import com.boldfaced7.board.domain.Member;
-import com.boldfaced7.board.dto.response.AuthResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,6 +22,8 @@ public class ArticleDto {
     private String author;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
+    private List<String> attachmentNames;
+    private List<String> attachmentUrls;
     private List<ArticleCommentDto> articleComments;
 
     public ArticleDto(Long articleId) {
@@ -46,6 +48,18 @@ public class ArticleDto {
         author = article.getMember().getNickname();
         createdAt = article.getCreatedAt();
         modifiedAt = article.getModifiedAt();
+        this.articleComments = articleComments.stream().map(ArticleCommentDto::new).toList();
+    }
+
+    public ArticleDto(Article article, List<ArticleComment> articleComments, List<String> attachmentUrls) {
+        articleId = article.getId();
+        memberId = article.getMember().getId();
+        title = article.getTitle();
+        content = article.getContent();
+        author = article.getMember().getNickname();
+        createdAt = article.getCreatedAt();
+        modifiedAt = article.getModifiedAt();
+        this.attachmentUrls = attachmentUrls;
         this.articleComments = articleComments.stream().map(ArticleCommentDto::new).toList();
     }
 

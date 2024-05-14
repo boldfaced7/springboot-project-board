c01e12b05664056781fc80ec44ccb6ff553565fd
boldfaced7 <boldfaced753@gmail.com>
2024-05-14 23:40:41 +0900

    #25 - 도메인 및 DTO 작성
    티켓 기능 도입을 위한 도메인을 추가하고, 해당 도메인의 데이터를 전달할 DTO를 작성

---
+++ src/main/java/com/boldfaced7/board/domain/ArticleTicket.java
@@ -0,0 +1,50 @@
+package com.boldfaced7.board.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@Entity
+public class ArticleTicket extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_ticket_id")
+    private Long id;
+
+    private boolean used = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    protected ArticleTicket() {}
+
+    @Builder
+    public ArticleTicket(Member member) {
+        this.member = member;
+    }
+
+    public boolean isAvailable() {
+        return LocalDate.now().isEqual(getCreatedAt().toLocalDate());
+    }
+    public void useTicket() {
+        used = true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleTicket that = (ArticleTicket) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

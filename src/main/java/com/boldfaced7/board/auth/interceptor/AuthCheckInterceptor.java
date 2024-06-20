21b274c72746a03d9e67479cbb85f6f20bcee5a1
boldfaced7 <boldfaced753@gmail.com>
2024-06-20 11:42:00 +0900

    #31 인터셉터 인터페이스 작성
    다형성을 사용한 JWT 적용을 위해 인터셉터 인터페이스 작성

--- src/main/java/com/boldfaced7/board/auth/interceptor/AuthCheckInterceptor.java
+++ src/main/java/com/boldfaced7/board/auth/interceptor/AuthCheckInterceptor.java
@@ -1,22 +1,9 @@
 package com.boldfaced7.board.auth.interceptor;
 
-import com.boldfaced7.board.auth.SessionConst;
-import com.boldfaced7.board.error.exception.auth.UnauthorizedException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public class AuthCheckInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        HttpSession session = request.getSession(false);
-
-        if (session == null || session.getAttribute(SessionConst.AUTH_RESPONSE) == null) {
-            throw new UnauthorizedException();
-        }
-
-        return true;
-    }
+public interface AuthCheckInterceptor extends HandlerInterceptor {
+    boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception;
 }
--- src/main/java/com/boldfaced7/board/auth/interceptor/LogoutSuccessInterceptor.java
+++ src/main/java/com/boldfaced7/board/auth/interceptor/LogoutSuccessInterceptor.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public interface AuthInfoHoldInterceptor extends HandlerInterceptor {
+public interface LogoutSuccessInterceptor extends HandlerInterceptor {
     boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception;
-    void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception;
 }

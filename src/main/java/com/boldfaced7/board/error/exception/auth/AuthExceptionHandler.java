43bea8e7a14325955876e82a223902e8812ecf0a
boldfaced7 <boldfaced753@gmail.com>
2024-06-20 11:44:51 +0900

    #31 - JWT 클래스 작성
    JWT 발급에 필요한 클래스와, 예외와 ExceptionHandler 작성

--- src/main/java/com/boldfaced7/board/error/exception/auth/AuthExceptionHandler.java
+++ src/main/java/com/boldfaced7/board/error/exception/auth/AuthExceptionHandler.java
@@ -3,10 +3,14 @@ package com.boldfaced7.board.error.exception.auth;
 import com.boldfaced7.board.error.ErrorCode;
 import com.boldfaced7.board.error.ErrorResponse;
 import com.boldfaced7.board.error.exception.GlobalExceptionHandler;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
 
 @RestControllerAdvice
 public class AuthExceptionHandler {
@@ -24,5 +28,12 @@ public class AuthExceptionHandler {
     @ExceptionHandler(InvalidAuthValueException.class)
     protected ResponseEntity<ErrorResponse> handle(InvalidAuthValueException e) {
         return GlobalExceptionHandler.createErrorResponseEntity(ErrorCode.INVALID_AUTH_VALUE);
+    }
+    @ExceptionHandler(InvalidRefreshTokenException.class)
+    protected ResponseEntity<Void> handle(InvalidRefreshTokenException e) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(URI.create("/api/login"));
+
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(httpHeaders).build();
     }
 }

f056d783674fe3416342dd7db9971cf2669837fc
boldfaced7 <boldfaced753@gmail.com>
2024-06-20 11:46:39 +0900

    #31 - AuthController 수정
    - 세션에 의존하지 않도록 수정
    - JWT 재발급 메소드 작성

--- src/main/java/com/boldfaced7/board/controller/AuthController.java
+++ src/main/java/com/boldfaced7/board/controller/AuthController.java
@@ -1,13 +1,14 @@
 package com.boldfaced7.board.controller;
 
+import com.boldfaced7.board.auth.AuthInfoHolder;
+import com.boldfaced7.board.auth.jwt.JwtProvider;
 import com.boldfaced7.board.dto.AuthDto;
 import com.boldfaced7.board.dto.request.AuthRequest;
 import com.boldfaced7.board.dto.response.AuthResponse;
-import com.boldfaced7.board.auth.SessionConst;
 import com.boldfaced7.board.service.AuthService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -18,28 +19,32 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final JwtProvider jwtProvider;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(
-            @RequestBody @Validated AuthRequest authRequest,
-            HttpSession session) {
-
+    public ResponseEntity<Void> login(@RequestBody @Validated AuthRequest authRequest) {
         AuthDto login = authService.login(authRequest.toDto());
         AuthResponse authResponse = new AuthResponse(login);
+        AuthInfoHolder.setAuthInfo(authResponse);
 
-        session.setMaxInactiveInterval(1800);
-        session.setAttribute(SessionConst.AUTH_RESPONSE, authResponse);
+        return ResponseEntity.status(HttpStatus.FOUND).build();
+    }
 
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate();
-        }
+    @GetMapping("/jwt")
+    public ResponseEntity<Void> refreshAccessToken(
+            @RequestHeader(JwtProvider.HEADER_AUTHORIZATION) String authorizationHeader,
+            @CookieValue(JwtProvider.REFRESH_TOKEN_COOKIE_NAME) String refreshToken) {
+        String accessToken = JwtProvider.extractToken(authorizationHeader);
+        String newAccessToken = jwtProvider.refreshAccessToken(accessToken, refreshToken);
 
-        return ResponseEntity.ok().build();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(JwtProvider.HEADER_AUTHORIZATION, newAccessToken);
+
+        return ResponseEntity.ok().headers(httpHeaders).build();
     }
 }
